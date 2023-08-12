c7e95dd0b6c980ea8c938a0fad4711ca6ad584ec
iqrahammad <135910831+iqrahammad@users.noreply.github.com>
2023-08-12 12:12:22 +0500

    js

---
+++ main.js
@@ -0,0 +1,34 @@
+var a = 20;
+a=100;
+console.log(a);
+console.log(typeof(a));
+
+let inc = 200;
+inc++;
+console.log(inc);
+console.log(typeof(inc));
+let dec = 200;
+dec--;
+console.log(dec);
+console.log(typeof(dec));
+
+
+let log=10>20 && 20>10;
+console.log(log);
+console.log(typeof(log));
+
+let log1=10>20 || 20>10;
+console.log(log1);
+console.log(typeof(log1));
+
+let alg = 10 + 4;
+console.log(alg);
+console.log(typeof(alg));
+
+let compare = inc>dec;
+console.log(compare);
+console.log(typeof(compare));
+
+let z;
+console.log(z);
+console.log(typeof(z));
